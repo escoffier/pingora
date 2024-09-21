55ac3d906685251fec24a0eba7b1447c20b44c74
qiuqunfeng <qiuqunfeng@tensorsecurity.cn>
2024-09-21 21:40:20 +0800

    update

--- pingora-core/src/listeners/inpod/mod.rs
+++ pingora-core/src/listeners/inpod/mod.rs
@@ -3,42 +3,42 @@ use nix::unistd::Pid;
 
 pub mod netns;
 
-pub fn new_netns(pid:Pid) -> OwnedFd {
-    let mut new_netns: Option<OwnedFd> = None;
-    std::thread::scope(|s| {
-        s.spawn(|| {
-            let res = nix::sched::unshare(CloneFlags::CLONE_NEWNET);
-            if res.is_err() {
-                return;
-            }
+// pub fn new_netns(pid:Pid) -> OwnedFd {
+//     let mut new_netns: Option<OwnedFd> = None;
+//     std::thread::scope(|s| {
+//         s.spawn(|| {
+//             let res = nix::sched::unshare(CloneFlags::CLONE_NEWNET);
+//             if res.is_err() {
+//                 return;
+//             }
 
-            if let Ok(newns) =
-                std::fs::File::open(format!("/proc/{}/ns/net", pid))
-            {
-                new_netns = Some(newns.into());
-            }
-        });
-    });
+//             if let Ok(newns) =
+//                 std::fs::File::open(format!("/proc/{}/ns/net", pid))
+//             {
+//                 new_netns = Some(newns.into());
+//             }
+//         });
+//     });
 
-    new_netns.expect("failed to create netns")
-}
+//     new_netns.expect("failed to create netns")
+// }
 
-pub fn new_netns1() -> OwnedFd {
-    let mut new_netns: Option<OwnedFd> = None;
-    std::thread::scope(|s| {
-        s.spawn(|| {
-            let res = nix::sched::unshare(CloneFlags::CLONE_NEWNET);
-            if res.is_err() {
-                return;
-            }
+// pub fn new_netns1() -> OwnedFd {
+//     let mut new_netns: Option<OwnedFd> = None;
+//     std::thread::scope(|s| {
+//         s.spawn(|| {
+//             let res = nix::sched::unshare(CloneFlags::CLONE_NEWNET);
+//             if res.is_err() {
+//                 return;
+//             }
 
-            if let Ok(newns) =
-                std::fs::File::open(format!("/proc/self/task/{}/ns/net", gettid()))
-            {
-                new_netns = Some(newns.into());
-            }
-        });
-    });
+//             if let Ok(newns) =
+//                 std::fs::File::open(format!("/proc/self/task/{}/ns/net", gettid()))
+//             {
+//                 new_netns = Some(newns.into());
+//             }
+//         });
+//     });
 
-    new_netns.expect("failed to create netns")
-}
+//     new_netns.expect("failed to create netns")
+// }
